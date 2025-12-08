46fc0b3d207dde6d3144742d839d5d8db82d3081
Dan Countryman <dan.r.countryman@gmail.com>
2025-12-08 14:11:37 -0600

    dt1

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-free-qr-code-generator",
+            },{id: "post-daily-tasks",
+        
+          title: "Daily Tasks",
+        
+        description: "Daily Tasks",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/DailyTasks/";
+          
+        },
+      },{id: "post-free-qr-code-generator",
         
           title: "Free QR Code Generator",
         
